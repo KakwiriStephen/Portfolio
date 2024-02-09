bc50a59e0c30e3ab8adbf1bfabf423c8e01c05bf
KakwiriStephen <ngottakakwiri@gmail.com>
2024-02-09 23:39:49 +0300

    slider

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,5 +1,7 @@
+import Swiper, { Pagination, Navigation } from "swiper";
 import gsap from "gsap";
-import Swiper from "swiper";
+
+import { reviews } from "./data";
 const bar = document.querySelector(".loading__bar--inner");
 const counter_num = document.querySelector(".loading__counter--number");
 
@@ -53,11 +55,43 @@ let barInterval = setInterval(() => {
     }
 }, 20);
 
+Swiper.use([Pagination, Navigation]);
 var swiper = new Swiper(".swiper", {
     slidesPerView: 1,
     spaceBetween: 30,
+    breakpoints: {
+        800: {
+            slidesPerView: 2,
+        },
+        1600: {
+            slidesPerView: 3,
+        },
+        1900: {
+            slidesPerView: 4,
+        },
+    },
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
     },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+const swiper_container = document.querySelector(".swiper-wrapper");
+reviews.map((review) => {
+    let template = `<div class="swiper-slide"> <div class="review"> <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-quote" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round" > <path stroke="none" d="M0 0h24v24H0z" fill="none" /> <path d="M10 11h-4a1 1 0 0 1 -1 -1v-3a1 1 0 0 1 1 -1h3a1 1 0 0 1 1 1v6c0 2.667 -1.333 4.333 -4 5" /> <path d="M19 11h-4a1 1 0 0 1 -1 -1v-3a1 1 0 0 1 1 -1h3a1 1 0 0 1 1 1v6c0 2.667 -1.333 4.333 -4 5" /> </svg> <div class="review__card"> <div class="review__topborder"></div> <div class="review__text"> <span>${review.review.substring(
+    0,
+    1
+  )}</span> ${review.review.substring(
+    1,
+    review.review.length
+  )} </div> <img src=${
+    review.image
+  } alt="" class="review__img" /> <div class="review__profile"> <span>${
+    review.name
+  }</span> <span>${review.position}</span> </div> </div> </div> </div>`;
+    swiper_container.innerHTML += template;
 });
---
+++ src/assets/js/data.js
@@ -0,0 +1,35 @@
+import images from "./images";
+
+let reviews = [{
+        name: "John Doe",
+        position: "client",
+        image: images.avatar1,
+        review: "As a freelancer, I found the software development community to be incredibly supportive. The flexibility of freelancing allowed me to take on diverse projects and expand my skill set.",
+    },
+    {
+        name: "Jane Smith",
+        position: "client",
+        image: images.avatar2,
+        review: "Software development as a freelancer has given me the opportunity to work on innovative projects and collaborate with clients from around the world. It's been a rewarding experience.",
+    },
+    {
+        name: "Alex Johnson",
+        position: "client",
+        image: images.avatar3,
+        review: "Freelancing in software development has allowed me to have a better work-life balance and pursue projects that truly interest me. It's a great way to grow professionally.",
+    },
+    {
+        name: "Emily Brown",
+        position: "client",
+        image: images.avatar4,
+        review: "I've found freelancing in software development to be a fulfilling career choice. The ability to work remotely and manage my own schedule has been invaluable.",
+    },
+    {
+        name: "Michael Lee",
+        position: "Data Scientist",
+        image: images.avatar5,
+        review: "The freelance software development world has provided me with the flexibility to explore different domains and apply my skills to a variety of interesting projects. It's been a fantastic journey.",
+    },
+];
+
+export { reviews };
