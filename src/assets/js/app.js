7d020411e20b26b463ebe9f8624e63692ec734d9
KakwiriStephen <ngottakakwiri@gmail.com>
2024-02-16 00:29:27 +0300

    Faq

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -98,3 +98,15 @@ reviews.map((review) => {
   }</span> <span>${review.position}</span> </div> </div> </div> </div>`;
     swiper_container.innerHTML += template;
 });
+
+const questions = [...document.querySelectorAll(".question")];
+questions.map((question) => {
+    let q_text = question.querySelector("h3");
+    q_text.addEventListener("click", () => {
+        questions
+            .filter((q) => q !== question)
+            .map((q) => q.classList.remove("open"));
+
+        question.classList.toggle("open");
+    });
+});
